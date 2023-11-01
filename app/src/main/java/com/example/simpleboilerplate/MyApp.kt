1a3acf666a53c96713d42e328e57a9671759a692
ldileh <ldileh@gmail.com>
2023-11-01 23:00:12 +0700

    changelog: implement timber on app

--- app/src/main/java/com/example/simpleboilerplate/MyApp.kt
+++ app/src/main/java/com/example/simpleboilerplate/MyApp.kt
@@ -1,10 +1,15 @@
 package com.example.simpleboilerplate
 
 import android.app.Application
+import timber.log.Timber
 
 class MyApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
