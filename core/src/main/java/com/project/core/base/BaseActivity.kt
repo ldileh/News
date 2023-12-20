559b502f49cb8f3588b7330f7acc6efaa213869b
ldileh <ldileh@gmail.com>
2023-12-20 17:55:34 +0700

    changelog: add new function to configure toolbar

--- core/src/main/java/com/project/core/base/BaseActivity.kt
+++ core/src/main/java/com/project/core/base/BaseActivity.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.project.core.utils.MessageUtil
+import com.skydoves.whatif.whatIfNotNull
 
 abstract class BaseActivity<T: ViewBinding>(): AppCompatActivity() {
 
@@ -19,12 +20,22 @@ abstract class BaseActivity<T: ViewBinding>(): AppCompatActivity() {
         _binding = getBindingFactory()(layoutInflater)
         setContentView(_binding.root)
 
+        toolbarId().whatIfNotNull {
+            setSupportActionBar(findViewById(it))
+            supportActionBar?.setDisplayHomeAsUpEnabled(isShowDisplayHome())
+        }
+
+
         _binding.onViewCreated(savedInstanceState)
     }
 
     abstract fun T.onViewCreated(savedInstanceState: Bundle?)
 
     abstract fun getBindingFactory(): (LayoutInflater) -> T
+
+    abstract fun toolbarId(): Int?
+
+    abstract fun isShowDisplayHome(): Boolean
 
     fun getBinding(): T = _binding
 }
