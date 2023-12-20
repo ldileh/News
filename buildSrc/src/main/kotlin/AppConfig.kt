4c4826506092c2540abb997bee0c28d2546162e9
ldileh <ldileh@gmail.com>
2023-12-20 16:58:03 +0700

    changelog: configure base url and api key

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -8,6 +8,8 @@ object AppConfig {
     const val versionCode = 1
     const val versionName = "0.0.1"
 
-    const val baseUrlProd = "\"https://api.github.com/\""
-    const val baseUrlDev = "\"https://api.github.com/\""
+    const val baseUrlProd = "\"https://newsapi.org/v2/\""
+    const val baseUrlDev = "\"https://newsapi.org/v2/\""
+
+    const val apiKey = "\"fd94af2885784592936b250e9a4ff418\""
 }
