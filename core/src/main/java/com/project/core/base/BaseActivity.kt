b29091bc38ac959b39e84b3497ed13561fe31fcc
ldileh <ldileh@gmail.com>
2023-12-22 10:38:36 +0700

    changelog: manage base activity from only support view binding to data binding

--- core/src/main/java/com/project/core/base/BaseActivity.kt
+++ core/src/main/java/com/project/core/base/BaseActivity.kt
@@ -2,9 +2,12 @@ package com.project.core.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.viewbinding.ViewBinding
 import com.project.core.utils.MessageUtil
+import com.skydoves.whatif.whatIf
 import com.skydoves.whatif.whatIfNotNull
 
 abstract class BaseActivity<T: ViewBinding>(): AppCompatActivity() {
@@ -17,7 +20,20 @@ abstract class BaseActivity<T: ViewBinding>(): AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = getBindingFactory()(layoutInflater)
+        whatIf(
+            given = isUseDataBinding(),
+            whatIf = {
+                getLayoutId().whatIfNotNull(
+                    whatIf = {
+                        _binding = DataBindingUtil.setContentView(this, it)
+                    },
+                    whatIfNot = {
+                        _binding = getBindingFactory()(layoutInflater)
+                    }
+                )
+            },
+            whatIfNot = { _binding = getBindingFactory()(layoutInflater) }
+        )
         setContentView(_binding.root)
 
         toolbarId().whatIfNotNull {
@@ -36,6 +52,11 @@ abstract class BaseActivity<T: ViewBinding>(): AppCompatActivity() {
     abstract fun toolbarId(): Int?
 
     abstract fun isShowDisplayHome(): Boolean
+
+    @LayoutRes
+    open fun getLayoutId(): Int? = null
+
+    open fun isUseDataBinding(): Boolean = false
 
     fun getBinding(): T = _binding
 }
